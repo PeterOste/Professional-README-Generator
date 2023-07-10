0dda780f23d0608a4e100d227f781eaa4e65dab6
unknown <peter.ostertag99@gmail.com>
2023-07-10 17:45:27 -0500

    Added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,35 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter a title for your project:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please enter the installation instructions for your project:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please enter usage instructions for your project:',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Please enter the license for your project:',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
